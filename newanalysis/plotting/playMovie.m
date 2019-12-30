2a09b94e349c2397d3bddaadbd7a1d460deb7ac8
d-curic <51892315+d-curic@users.noreply.github.com>
2019-12-30 15:55:09 -0700

    Update playMovie.m
    Added in a section to deal with the case of a frame having only non-zero pixel values.

--- newanalysis/plotting/playMovie.m
+++ newanalysis/plotting/playMovie.m
@@ -6,20 +6,38 @@ numdimensions = ndims(frames);
 switch numdimensions
     case 3
         for i = 1:size(frames,3)
-        %imshow(label2rgb(frames(:,:,i)))
-        imagesc(frames(:,:,i))
-        title(num2str(i))
-        drawnow
+            %imshow(label2rgb(frames(:,:,i)))
+            imagesc(frames(:,:,i))
+            title(num2str(i))
+            drawnow
         end
     case 2
-        frames = reshape(frames,exp.siz(1),exp.siz(2),exp.numframes);
-        for i = 1:size(frames,3)
-        %imshow(label2rgb(frames(:,:,i)))
-        imagesc(frames(:,:,i))
-        title(num2str(i))
-        drawnow
+        
+        %sometimes the frame data contains only values from the non-zero
+        %pixel values. In this case a new frame must be made to accomodate
+        %this. This is handled in the else statement.
+        
+        if prod(size(frames)) == prod(size(exp.mask))*exp.numframes
+            frames = reshape(frames,exp.siz(1),exp.siz(2),exp.numframes);
+            for i = 1:size(frames,3)
+                %imshow(label2rgb(frames(:,:,i)))
+                imagesc(frames(:,:,i))
+                title(num2str(i))
+                drawnow
+            end
+            
+        else
+            
+            
+            for i = 1:exp.numframes
+                frame = zeros(exp.siz);
+                frame(exp.validPixelIdx) = frames(:,i);
+                imagesc(frame)
+                title(num2str(i))
+                drawnow
+            end
+
         end
-       
 end
 
 
