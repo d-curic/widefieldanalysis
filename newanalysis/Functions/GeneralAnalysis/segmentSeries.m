9411821308e086069bc648c112a86098af7ac2f4
d-curic <51892315+d-curic@users.noreply.github.com>
2020-01-02 19:27:42 -0700

    Update segmentSeries.m
    fixed a bug that occurs if start and end of list are 0s

--- newanalysis/Functions/GeneralAnalysis/segmentSeries.m
+++ newanalysis/Functions/GeneralAnalysis/segmentSeries.m
@@ -1,10 +1,12 @@
 
 function [section, times] = segmentSeries(A)
 ne0 = find(A~=0);                                   % Nonzero Elements
-ix0 = unique([ne0(1) ne0(diff([0 ne0])>1)]);        % Segment Start Indices
-ix1 = ne0([find(diff([0 ne0])>1)-1 length(ne0)]);   % Segment End Indices
-for k1 = 1:length(ix0)
-    section{k1} = A(ix0(k1):ix1(k1));
-    times{k1} = ix0(k1):ix1(k1);
+ix0 = unique([ne0(1) ne0(diff([0 ne0])>1)]);        % Non-Zero Segment Start Indices
+eq0 = find(A==0);                                   % Zero Elements
+ix1 = unique([eq0(1) eq0(diff([0 eq0])>1)]);        % Zero Segment Start Indices
+ixv = sort([ix0 ix1 length(A)]);                    % Consecutive Indices Vector
+for k1 = 1:length(ixv)-1
+    section{k1} = A(ixv(k1):ixv(k1+1)-1);
+    times{k1} = ixv(k1):ixv(k1+1)-1;
 end
 end
